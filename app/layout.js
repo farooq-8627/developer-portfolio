c30aea73c301d45333585a949968b29f4e2f1a48
UMAR FAROOQ SHAIK <umarfarooq8226@gmail.com>
2025-02-20 15:38:55 +0530

    Favicon

--- app/layout.js
+++ app/layout.js
@@ -13,37 +13,14 @@ export const metadata = {
 	title: "Portfolio of Umar Farooq - Software Developer",
 	description:
 		"This is the portfolio of Umar Farooq. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others.",
-	icons: {
-		icon: [
-			{
-				url: "/og-image.png",
-				sizes: "any",
-			},
-		],
-	},
-	manifest: "/site.webmanifest",
-	openGraph: {
-		title: "Portfolio of Umar Farooq - Software Developer",
-		description:
-			"Full Stack Developer specializing in web and mobile development",
-		url: "https://farooqs-portfolio.vercel.app",
-		siteName: "Umar Farooq Portfolio",
-		images: [
-			{
-				url: "/og-image.png",
-				width: 1200,
-				height: 630,
-				alt: "Umar Farooq - Software Developer",
-			},
-		],
-		locale: "en_US",
-		type: "website",
-	},
 };
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
+			<head>
+				<link rel="icon" href="/og-image.png" />
+			</head>
 			<body className={inter.className}>
 				<ToastContainer />
 				<main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
