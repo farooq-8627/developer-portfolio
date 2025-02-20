66ddd5b1e109b8344c8379fde7f6dc4a47389dde
UMAR FAROOQ SHAIK <umarfarooq8226@gmail.com>
2025-02-20 15:33:06 +0530

    Favicon

--- app/layout.js
+++ app/layout.js
@@ -11,9 +11,34 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
 	title: "Portfolio of Umar Farooq - Software Developer",
-	icon: "/favicon.ico",
 	description:
 		"This is the portfolio of Umar Farooq. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others.",
+	icons: {
+		icon: [
+			{
+				url: "/og-image.png",
+				sizes: "any",
+			},
+		],
+	},
+	manifest: "/site.webmanifest",
+	openGraph: {
+		title: "Portfolio of Umar Farooq - Software Developer",
+		description:
+			"Full Stack Developer specializing in web and mobile development",
+		url: "https://farooqs-portfolio.vercel.app",
+		siteName: "Umar Farooq Portfolio",
+		images: [
+			{
+				url: "/og-image.png",
+				width: 1200,
+				height: 630,
+				alt: "Umar Farooq - Software Developer",
+			},
+		],
+		locale: "en_US",
+		type: "website",
+	},
 };
 
 export default function RootLayout({ children }) {
