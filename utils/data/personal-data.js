3014c54cadb7d3700c6ebd0ad6c1b80e0332b4df
UMAR FAROOQ SHAIK <umarfarooq8226@gmail.com>
2025-02-27 18:46:46 +0530

    Update Profile Picture

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
 	name: "UMAR FAROOQ",
-	profile: "/about.png",
+	profile: "/about.webp",
 	designation: "Software Developer",
 	description:
 		"My name is UMAR FAROOQ. I’m a passionate Full Stack Web and Mobile Developer with a strong foundation in web and mobile technologies. I thrive on learning new tools and techniques, delivering creative and efficient solutions. As a quick learner with a self-driven attitude, I love exploring new technologies and solving complex problems. I am a professional and enthusiastic programmer in my daily life. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
